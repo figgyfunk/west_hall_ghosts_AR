e8ebbd038c6b7d458491bd33e1bbb1c0aea21bb8
figgyfunk <figa@rpi.edu>
2018-09-13 21:42:01 -0400

    Messing around with pokemon cards.

---
+++ west_hall_research_project/Assets/TargetPlayAudio.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TargetPlayAudio : MonoBehaviour,
+                                            ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+    private bool foundOnce = false;
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            if (foundOnce)
+            {
+                GetComponent<AudioSource>().UnPause();
+            }
+            // Play audio when target is found
+            else
+            {
+                GetComponent<AudioSource>().Play();
+            }
+            
+        }
+        else
+        {
+            // Stop audio when target is lost
+            GetComponent<AudioSource>().Pause();
+        }
+    }
+}
+
